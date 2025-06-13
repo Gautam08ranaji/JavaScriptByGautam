81de6738abd513979ae708344a1e5a4ba5d7c89c
GAUTAM RANA <114357310+Gautam08ranaji@users.noreply.github.com>
2025-06-13 05:04:07 +0000

    Maths and num

---
+++ 01_basics/06_nums-and-maths.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score);
+
+const num = new Number(100.9876)
+// console.log(num);
+
+
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(3));
+
+// console.log(score);
+// console.log(score.valueOf());
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// --------------------------MATHS--------------------------
+
+// console.log(Math);
+// console.log(Math.SQRT2);
+// console.log(Math.abs(-23));
+// console.log(Math.round(23.165));
+// console.log(Math.ceil(23.165));
+// console.log(Math.floor(23.999));
+// console.log(Math.min(4,6,62,45,65));
+// console.log(Math.max(4,6,62,45,65));
+
+// console.log(Math.floor((Math.random()*1000)+1));
+
+const min = 100000
+const max = 999999
+
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
