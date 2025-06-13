c34cb3a5baac25b53495f858accd48694e12caa4
GAUTAM RANA <114357310+Gautam08ranaji@users.noreply.github.com>
2025-06-13 05:52:59 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDate());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// console.log( myDate.getMonth());
+
+
